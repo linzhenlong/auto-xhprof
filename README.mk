41e5ba297868827778adea55db1992e3cce9d509
smallfish <smallfish.xy@gmail.com>
2011-09-13 12:35:18 +0800

    Update README

---
+++ README.mk
@@ -0,0 +1,18 @@
+======
+auto xhprof
+======
+
+通过设置php.ini中的auto_prepend_file，加载auto-xhprof，保存数据到MySQL。
+
+常量的定义请参考auto-xhprof.php中define部分。
+
+页面展示部分请修改index.php/callgraph.php中$xhprof_runs_impl实例部分，修改成XHProfRuns_MySQL。
+
+xhprof表结构：
+
+CREATE TABLE `xhprof` (
+`run_id` varchar(64) DEFAULT NULL,
+`type` varchar(256) DEFAULT NULL,
+`data` text,
+`optime` datetime DEFAULT NULL
+);
